07761ba380575d69c7a7ba613bd4ebd004dae316
potato <mbarkimoumed@outlook.com>
2024-06-04 00:00:24 +0100

    update

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-Web Dev Project made by Mbarkiou Mohamed and benhamout mohamed.
-npm start to start the project. then go to http://localhost:5000/
-You also need the mongodb datatbase node with users collection
+webdev
--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+const HTTPS_PORT = 8443;
+
 require("dotenv").config();
 const { error } = require("console");
 const express = require("express");
 const session = require("express-session");
 const mongoose = require("mongoose");
+const fs = require("fs");
+const https = require("https");
+const WebSocket = require("ws");
+const WebSocketServer = WebSocket.Server;
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+
+const rooms = new Map();
 
 mongoose.connect(process.env.DB_URI);
 
@@ -13,27 +20,106 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to DB"));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const serverConfig = {
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem"),
+};
 
-app.use(
-  session({
-    secret: "my secret key",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
+function main() {
+    const httpsServer = startHttpsServer(serverConfig);
+    startWebSocketServer(httpsServer);
+    printHelp();
+}
 
-app.use((req, res, next) => {
-  res.locals.message = req.session.message;
-  delete req.session.message;
-  next();
-});
+function startHttpsServer(serverConfig) {
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
-app.set("view engine", "ejs");
+    app.use(
+        session({
+            secret: "my secret key",
+            resave: false,
+            saveUninitialized: false,
+        })
+    );
 
-app.use("", require("./routes/routes"));
+    app.use((req, res, next) => {
+        res.locals.message = req.session.message;
+        delete req.session.message;
+        next();
+    });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+    app.set("view engine", "ejs");
+
+    app.use("", require("./routes/routes"));
+
+    const httpsServer = https.createServer(serverConfig, app);
+    httpsServer.listen(HTTPS_PORT, "0.0.0.0");
+    return httpsServer;
+}
+
+function startWebSocketServer(httpsServer) {
+    // Create a server for handling websocket calls
+    const wss = new WebSocketServer({ server: httpsServer });
+
+    wss.on("connection", (ws) => {
+        ws.on("message", (message) => {
+            // Broadcast any received message to all clients
+            const data = JSON.parse(message);
+
+            if (data.roomcode) {
+                console.log(`Received room code: ${data.roomcode}`);
+                // Add the client to the room
+                if (!rooms.has(data.roomcode)) {
+                    rooms.set(data.roomcode, []);
+                }
+                rooms.get(data.roomcode).push(ws);
+            } else {
+                console.log(`received: ${message}`);
+                // Broadcast the message to all clients in the same room
+                const room = findRoom(ws);
+                if (room) {
+                    broadcast(room, message);
+                }
+            }
+        });
+    });
+}
+
+function findRoom(ws) {
+    for (const [roomcode, clients] of rooms.entries()) {
+        if (clients.includes(ws)) {
+            return roomcode;
+        }
+    }
+    return null;
+}
+
+function broadcast(roomcode, data) {
+    const clients = rooms.get(roomcode);
+    if (clients) {
+        clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data, { binary: false });
+            }
+        });
+    }
+}
+
+function printHelp() {
+    console.log(
+        `Server running. Visit https://localhost:${HTTPS_PORT} in Firefox/Chrome/Safari.\n`
+    );
+    console.log("Please note the following:");
+    console.log(
+        "  * Note the HTTPS in the URL; there is no HTTP -> HTTPS redirect."
+    );
+    console.log(
+        "  * You'll need to accept the invalid TLS certificate as it is self-signed."
+    );
+    console.log(
+        "  * Some browsers or OSs may not allow the webcam to be used by multiple pages at once. You may need to use two different browsers or machines."
+    );
+}
+
+main();
